3de74eb5339bb3089bd45e716599e1f17b5a978e
shiweisong <shiweisongssw@foxmail.com>
2018-11-13 11:43:52 +0800

    network coding

---
+++ TCP_sample/browse_web.py
@@ -0,0 +1,22 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('lib.nwu.edu.cn', 80))
+
+s.send(b'GET  / HTTP/1.1\r\nHost: lib.nwu.edu.cn\r\nConnection: close\r\n\r\n')
+
+buffer = []
+while True:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = b''.join(buffer)
+
+s.close()
+
+header, html = data.split(b'\r\n\r\n', 1)
+print(header.decode('utf-8'))
+with open('obtained.html', 'wb') as f:
+    f.write(html)
